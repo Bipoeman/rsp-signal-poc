aef38d0afbf649851e9f2987f30d9614d9393687
Chanathip <bipoeman12@gmail.com>
2023-12-05 14:41:28 +0700

    Bode plot available in automateTest.py

--- automateTest.py
+++ automateTest.py
@@ -1,13 +1,14 @@
 import pyvisa
 import time
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+import instrumentData
+
 rm = pyvisa.ResourceManager()
 print(rm.list_resources())
-function_gen = rm.open_resource('USB0::0x049F::0x505B::CN2025006001104::0::INSTR')
-# scope = rm.open_resource('TCPIP0::192.168.28.18::inst0::INSTR')
-scope = rm.open_resource('USB0::0xF4EC::0xEE38::SDSMMEBC3R1349::0::INSTR')
-import matplotlib.pyplot as plt
-
-import numpy as np
+function_gen = rm.open_resource(instrumentData.function_gen_id)
+scope = rm.open_resource(instrumentData.scope_id)
 
 def cal_freq_range(freq_min,freq_max,num_point):
     logStart = np.log10(freq_min)
@@ -33,6 +34,9 @@ def float_vdiv(msg : str):
 def float_tdiv(msg : str):
     return float(msg.split(" ")[1].split("S")[0])
 
+def float_phase(msg : str):
+    return float(msg.split(",")[1].split("degree")[0])
+
 def right_vdiv_for_pkpk(channel : str):
     
     lenArr = 12
@@ -49,17 +53,17 @@ def right_vdiv_for_pkpk(channel : str):
     v_div_index = v_div_available.index(v_div)
     # for i in range(12):
         
-    print(v_div_available)
-    print("v_val : ",v_val)
-    print(v_div * num_grid_vert)
+    # print(v_div_available)
+    # print("v_val : ",v_val)
+    # print(v_div * num_grid_vert)
     if (v_val > v_div * num_grid_vert * 0.9):
-        print("It's over scale")
+        # print("It's over scale")
         for i in range(v_div_index,12):
             pkpk = scope.query(f"{channel}:PAVA? PKPK")
             pkpk = float_pava_pkpk(pkpk)
             vdiv = scope.query(f"{channel}:VDIV?")
             vdiv = float_vdiv(vdiv)
-            print(f"index : {i}, pkpk : {pkpk} , max : {vdiv * num_grid_vert}")
+            # print(f"index : {i}, pkpk : {pkpk} , max : {vdiv * num_grid_vert}")
             if (pkpk > vdiv * num_grid_vert * 0.9):
                 scope.write(f"{channel}:VDIV {v_div_available[i]}")
             else:
@@ -71,7 +75,7 @@ def right_vdiv_for_pkpk(channel : str):
             pkpk = float_pava_pkpk(pkpk)
             vdiv = scope.query(f"{channel}:VDIV?")
             vdiv = float_vdiv(vdiv)
-            print(f"index : {i}, pkpk : {pkpk} , max : {vdiv * num_grid_vert}")
+            # print(f"index : {i}, pkpk : {pkpk} , max : {vdiv * num_grid_vert}")
             if (pkpk < vdiv * 2.5):
                 scope.write(f"{channel}:VDIV {v_div_available[i]}")
             else:
@@ -101,22 +105,22 @@ def right_time_div(channel : str):
                 scope.write(f"TDIV {i}")
                 break   
         break
-    print(freq)
-    print(time_div)
+    # print(freq)
+    # print(time_div)
     
 def right_time_div_ext_freq(channel : str,freq : float):
     num_grid_hori = 7
     time_div_num = [1e-9,2e-9,5e-9]
     time_div_available : list = [float(f"{time_div_num[i % 3] * 10**((i)//3):.9f}") for i in range(32)]
     time_div = float_tdiv(scope.query(f"TDIV?"))
-    print(time_div_available)
+    # print(time_div_available)
     T = 1/freq
     for i in time_div_available:
         if T < i * num_grid_hori and T > i * (num_grid_hori/2):
             scope.write(f"TDIV {i}")
             break   
-    print(freq)
-    print(time_div)
+    # print(freq)
+    # print(time_div)
 
 
 
@@ -125,59 +129,62 @@ def right_time_div_ext_freq(channel : str,freq : float):
         
 
 # print(vpk)
-# freq = 1700
-freqArr = cal_freq_range(20,20000,100)
-inArr = []
-outArr = []
-fileName = '100_Q0.5'
+# freq = 1700#
+
+###################### Buzzer Off before measurement ######################
+## BUZZ ON / OFF
+
+
+
+freqArr = cal_freq_range(1000,1E6,200)
+fileName = 'rlcFilterAgain'
+# scope.write("MEAsure_Delay PHA,C1-C2")
+
+
+# phase = scope.query("C1-C2:MEAD? PHA")
+# print(phase)
+
+
+
 with open(f'./EasyEQ test results/{fileName}.csv', 'w') as f:
     f.write('')
 
 # print(freqArr)
 
-
 for freq in freqArr:
     function_gen.write(f'SOURce1:FREQ {freq}')
     
     # right_time_div("C1")
+    print(f"Frequency : {freq}", end = "... ")
     right_time_div_ext_freq("C1",freq)
     right_vdiv_for_pkpk("C1")
     right_vdiv_for_pkpk("C2")
-    time.sleep(0.2)
+    print(np.exp(freq * -0.05),end=" ")
+    time.sleep(np.exp(freq * -0.05))
     vin = float_pava_pkpk(scope.query("C1:PAVA? PKPK"))
     vout = float_pava_pkpk(scope.query("C2:PAVA? PKPK"))
-    # inArr.append()
-    # outArr.append()
-    print(f'{freq},{vout/vin}',file=open(f'./EasyEQ test results/{fileName}.csv', 'a'))
+    phase = float_phase(scope.query("C1-C2:MEAD? PHA"))
+    print(f'{freq},{vout/vin},{phase}',file=open(f'./EasyEQ test results/{fileName}.csv', 'a'))
+    print(f"Done")
 
-# function_gen.write(f'SOURce1:FREQ {freq}')
-# for j in range(4):
-#     for i in range(9):
-#         try:
-#             freq = (i + 1) * (10 ** j)
-#             function_gen.write(f'SOURce1:FREQ {freq}')
-#             sumvpk = 0
-#             sumvin = 0
-#             vpk = 0
-#             vin = 0
-#             for i in range(10):
-#                 vpk : str = scope.query("C1:PAVA? PKPK")
-#                 vin : str = scope.query("C2:PAVA? PKPK")
-#                 print(vpk,vin)
-#                 vpk : int = float(vpk.split(',')[1].split('V')[0])
-#                 vin : int = float(vin.split(',')[1].split('V')[0])
-#                 sumvin += vin
-#                 sumvpk += vpk
-#                 time.sleep(0.2) 
-#             sumvin = sumvin / 10
-#             sumvpk = sumvpk / 10
-#             # inArr.append(sumvin)
-#             # outArr.append(sumvpk)
-#             # freqArr.append(freq)
-#             # print(f'Frequency : {(i + 1) * 100} Ratio : {vpk/vin}')
-#             print(f'{freq},{sumvpk/sumvin}',file=open('inout.csv', 'a'))
-#         except Exception as e:
-#             print(e)
-#         time.sleep(0.05)
-# plt.plot(freq,inArr)
-# print(scope.query('*IDN?'))
+freq = []
+mag = []
+fig,ax = plt.subplots()
+with open(f'./EasyEQ test results/{fileName}.csv','r') as file:
+    print("File openned")
+    reader = csv.reader(file)
+    for row in reader:
+        # freq.append(10**(float(row[0])/20))
+        freq.append(float(row[0]))
+        mag.append(20*np.log10(float(row[1])))
+ax.set_xscale("log")
+# ax.set_xlim([20,20e3])
+# ax.set_ylim([-15,15])
+ax.set_ylabel("Gain (db)")
+ax.set_xlabel("Frequency (Hz)")
+ax.set_title("EasyEQ Frequency Response")
+ax.grid(True, which="both", ls="-")
+ax.plot(freq,mag)
+fig.savefig(f"./EasyEQ test results/{fileName}.svg")
+fig.savefig(f"./EasyEQ test results/{fileName}.png")
+plt.show()
